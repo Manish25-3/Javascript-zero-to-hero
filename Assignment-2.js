dcabf888e40513d5a21552ae9c6b0b62d537a6f1
Manish25-3 <84845960+Manish25-3@users.noreply.github.com>
2021-09-08 23:46:57 +0530

    Add files via upload
    Assignment 2- day 2 - SUBMITTED

---
+++ Assignment-2.js
@@ -0,0 +1,17 @@
+let arr = [4,5,7,8,14,45,56,73,44,76];
+for(let i=0;i<10;i++)
+    { if(arr[i]%2 == 0)
+            console.log(arr[i]);
+    } 
+
+/*
+OUTPUT :
+
+C:\Users\ASUS\Desktop\Javascript - LetsUpgrade>node Assignment-2.js
+4
+8
+14
+56
+44
+76
+*/
