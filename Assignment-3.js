467d925d2b2458a3d6b9bf40cfcd784836ed9850
Manish25-3 <84845960+Manish25-3@users.noreply.github.com>
2021-09-09 00:17:52 +0530

    Add files via upload
    Assignment3- day 3- submitted

---
+++ Assignment-3.js
@@ -0,0 +1,10 @@
+let arr = ["Mangalore","Mulki","Kinnigoli","Kateel","Bappanad"];
+
+console.log("Array elements in Normal order : ")
+for(let i=0;i<5;i--)
+    console.log(arr[i]);
+
+console.log("Array elements in Reverse order : ");    
+    for(let i;i>0;i--)
+        console.log(arr[i]);
+            
